51bb06f0937de30f23cab68fa864b42ae31be1fc
Jordan <moy27pogi@gmail.com>
2018-07-14 15:50:55 +0800

    Generating reports

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -9,9 +9,13 @@ use PDF;
 class ReportsController extends Controller
 {
     public function hiredApplicantReport(Request $request) {
-        PDF::SetTitle('Hello World');
+    	$hired = Applicant::where('hired', 1)->get();
+        $view = \View::make('hired_applicants_report')->with(array('data' => $hired));
+        $html = $view->render();
+     
+        PDF::SetTitle('Hired Applicants');
         PDF::AddPage();
-        PDF::Write(0, 'Hello World');
-        PDF::Output();
+        PDF::writeHTML($html);
+        PDF::Output('hired_applicants_report.pdf', 'I');
     }
 }
