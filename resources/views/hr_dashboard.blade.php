98eb92e719d2cd6a28ff36caf1f224b9cccac135
Charles Marnie Limpo <charlesmarnielimpo@gmail.com>
2018-08-06 13:37:32 +0800

    Dashboard

--- resources/views/hr_dashboard.blade.php
+++ resources/views/hr_dashboard.blade.php
@@ -15,19 +15,7 @@
 
     <section class="summary">
       <div class="row">
-        <div class="col-lg-3 col-md-3 col-sm-12 mb-3">
-          <div class="card">
-            <div class="card-body">
-              <div class="d-flex justify-content-between align-items-center mb-4">
-                <h6 class="card-title mb-0">Total Applicant</h6>
-                <i class="fas fa-list fa-2x"></i>
-              </div>
-              <h1 class="text-center mb-0 display-3">0</h1>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-3 col-md-3 col-sm-12 mb-3">
+        <div class="col-lg-4 col-md-4 col-sm-12 mb-3">
           <div class="card">
             <div class="card-body">
               <div class="d-flex justify-content-between align-items-center mb-4">
@@ -39,7 +27,7 @@
           </div>
         </div>
 
-        <div class="col-lg-3 col-md-3 col-sm-12 mb-3">
+        <div class="col-lg-4 col-md-4 col-sm-12 mb-3">
           <div class="card">
             <div class="card-body">
               <div class="d-flex justify-content-between align-items-center mb-4">
@@ -51,7 +39,7 @@
           </div>
         </div>
 
-        <div class="col-lg-3 col-md-3 col-sm-12 mb-3">
+        <div class="col-lg-4 col-md-4 col-sm-12 mb-3">
           <div class="card">
             <div class="card-body">
               <div class="d-flex justify-content-between align-items-center mb-4">
