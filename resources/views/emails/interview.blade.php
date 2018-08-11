1e9200c70a27bdd893d6931a6c0b70c24cb711c0
Charles Marnie Limpo <charlesmarnielimpo@gmail.com>
2018-08-11 14:23:53 +0800

    Interview Email

--- resources/views/emails/interview.blade.php
+++ resources/views/emails/interview.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # {{ $title }}
 
-{{ $message }} <strong>{{ $interviewDate }}</strong> at <strong>{{ $interviewTime }}</strong>.
+{{ $message }} <strong>{{ $interviewDate }}</strong> at <strong>{{ $interviewTime }}</strong>. Please come as early as possible.
 
 Thanks,<br>
 {{ config('app.name') }}
