9ef171301c65aac5bae8b4420836424d88b7216a
Charles Marnie Limpo <charlesmarnielimpo@gmail.com>
2018-08-19 19:13:42 +0800

    Ready for Deployment

--- resources/views/approved_applicants.blade.php
+++ resources/views/approved_applicants.blade.php
@@ -90,54 +90,6 @@
       </div>
     @endforelse
 
-    <!-- Applicant Profile Modal -->
-    <div class="modal fade" id="applicant-profile" tabindex="-1" role="dialog" aria-labelledby="applicantProfileLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="applicantProfileLabel">Applicant Profile</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <div id="image" class="text-center mb-3">
-              <img src="{{ asset(App::environment('production') ? '/public/img/patton-logo.png' : '/img/patton-logo.png') }}" alt="John Doe" title="John Doe" class="img-thumbnail" id="image-profile" width="120px" height="120px"></div>
-            <dl class="row">
-              <dt class="col-sm-4">Name:</dt>
-              <dd class="col-sm-8" id="name"></dd>
-              <dt class="col-sm-4">Applying for:</dt>
-              <dd class="col-sm-8" id="job"></dd>
-              <dt class="col-sm-4">Age:</dt>
-              <dd class="col-sm-8" id="age"></dd>
-              <dt class="col-sm-4">Gender:</dt>
-              <dd class="col-sm-8" id="gender"></dd>
-              <dt class="col-sm-4">Address:</dt>
-              <dd class="col-sm-8" id="address"></dd>
-              <dt class="col-sm-4">Contact Number:</dt>
-              <dd class="col-sm-8" id="mobile"></dd>
-              <dt class="col-sm-4 dd-interview-date">Interview Date:</dt>
-              <dd class="col-sm-8 dd-interview-date" id="interview-date"></dd>
-              <dt class="col-sm-4 dd-interview-time">Interview Time:</dt>
-              <dd class="col-sm-8 dd-interview-time" id="interview-time"></dd>
-              <dt class="col-sm-4 dd-interview-time">Interviewed?</dt>
-              <dd class="col-sm-8 dd-interviewed" id="interviewed">Yes</dd>
-              <dt class="col-sm-4 dd-interview-time">Score:</dt>
-              <dd class="col-sm-8 dd-score" id="score"></dd>
-              <dt class="col-sm-4 dd-training-date">Training Date:</dt>
-              <dd class="col-sm-8 dd-training-date" id="training-date"></dd>
-              <dt class="col-sm-4 dd-date-approved">Date Approved:</dt>
-              <dd class="col-sm-8 dd-date-approved" id="date-approved"></dd>
-            </dl>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-warning" id="btn-hiring">Hire</button>
-          </div>
-        </div>
-      </div>
-    </div>
-
     <!-- Hire Modal -->
     <div class="modal fade" id="hire-applicant" tabindex="-1" role="dialog" aria-labelledby="hireApplicantLabel" aria-hidden="true">
       <div class="modal-dialog" role="document">
